9270c09a8a34f22fc60252ef695bdc5ef8f89ac0
Jens Deppe <jdeppe@gopivotal.com>
2014-01-16 13:15:22 -0800

    Added a few more tasks to cycle the server and load the schema. Minor tweaks to load the Loader as part of the webapp startup.

--- gfxd-demo-web/src/main/java/com/pivotal/gfxd/demo/Demo.java
+++ gfxd-demo-web/src/main/java/com/pivotal/gfxd/demo/Demo.java
@@ -1,21 +1,25 @@
 package com.pivotal.gfxd.demo;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.pivotal.gfxd.demo.loader.LoadRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
 
 /**
  * @author Jens Deppe
  */
-@Controller
-@RequestMapping("/data")
+@Component
 public class Demo {
-  @RequestMapping(value = "/foo", method = RequestMethod.GET)
-  @ResponseBody
-  public ResponseEntity provisionServiceInstance() {
-    return new ResponseEntity("Hello World", HttpStatus.OK);
+
+  private static final Logger LOG = Logger.getLogger(Demo.class.getName());
+
+  @Autowired
+  private LoadRunner runner;
+
+  @PostConstruct
+  private void run() {
+    LOG.info("-------->>>>> YAH!!!!");
   }
 }
---
+++ scripts/schema.sql
@@ -0,0 +1,53 @@
+drop table if exists raw_sensor;
+create table raw_sensor
+  (
+    id bigint,
+    timestamp bigint,
+    value float(23),
+    property smallint,
+    plug_id integer,
+    household_id integer,
+    house_id integer
+--    CONSTRAINT RAW_SENSOR_PK PRIMARY KEY (ID)
+  )
+  partition by column (house_id)
+  eviction by lrucount 1000000
+  evictaction destroy
+--  persistent asynchronous
+;
+
+-- ALTER TABLE FLIGHTAVAILABILITY
+--    ADD CONSTRAINT FLIGHTAVAIL_PK Primary Key (
+--       FLIGHT_ID,
+--       SEGMENT_NUMBER,
+--       FLIGHT_DATE);
+
+drop table if exists load_averages;
+create table load_averages
+  (
+    house_id integer,
+    household_id integer,
+    plug_id integer,
+    weekday smallint,
+    time_segment smallint,
+    total_load float(23),
+    event_count integer
+  )
+  partition by column (house_id)
+;
+
+drop index if exists load_averages_idx;
+create index load_averages_idx on load_averages (house_id, household_id, plug_id, weekday, time_segment);
+
+drop asynceventlistener if exists AggListener;
+-- create asynceventlistener AggListener
+-- (
+--    listenerclass 'com.pivotal.gfxd.demo.AggregationListener'
+--    initparams ''
+--    batchsize 1000
+--    batchtimeinterval 1000
+-- ) server groups (group1);
+-- 
+-- alter table raw_sensor set asynceventlistener (AggListener);
+-- 
+-- call sys.start_async_event_listener('AggListener');
