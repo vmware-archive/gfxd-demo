4b937110b5d9c6d2f473ebfd4a67b6da14353aa4
Jens Deppe <jdeppe@gopivotal.com>
2014-01-30 10:33:40 -0800

    Tweaked setupLoadAveragesSql to use property expansion instead of Ant tokens and also to always run

--- scripts/import_load_averages.sql
+++ scripts/import_load_averages.sql
@@ -1,6 +1,6 @@
 connect client 'localhost:1527';
 
-CALL SYSCS_UTIL.IMPORT_TABLE_EX ('APP', 'LOAD_AVERAGES', '@DATA_TOKEN@', ',', null, null, 0, 
+CALL SYSCS_UTIL.IMPORT_TABLE_EX ('APP', 'LOAD_AVERAGES', '${load_averages_csv}', ',', null, null, 0, 
  0 /* don't lock the table */,
  6 /* threads to use for import */,
  0 /* case insensitive table name */,
